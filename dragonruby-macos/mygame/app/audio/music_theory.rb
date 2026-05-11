bd47936525b4c2a65e9aad8e1784b76dea61e093
Andrea D'Amico <andrea.damico@gmail.com>
2026-05-11 08:33:13 +0200

    fix(audio): add missing enharmonic equivalents in semitones mapping

--- dragonruby-macos/mygame/app/audio/music_theory.rb
+++ dragonruby-macos/mygame/app/audio/music_theory.rb
@@ -3,11 +3,16 @@ module Audio
     SAMPLE_RATE = 48_000
     SEMITONES_FROM_A = {
       c: -9,
+      cs: -8, db: -8,
       d: -7,
+      ds: -6, eb: -6,
       e: -5,
       f: -4,
+      fs: -3, gb: -3,
       g: -2,
+      gs: -1, ab: -1,
       a: 0,
+      as: 1,  bb: 1,
       b: 2,
     }.freeze
     SUPPORTED_OCTAVES = (0..8).freeze
